5fd8341eecd7165eb2f0acbb05937d3cb062b3ca
wallcrawler <ronan_mccabe@hotmail.com>
2015-05-22 18:27:47 +0100

    changes

--- testing/tests.js
+++ testing/tests.js
@@ -1,16 +1,16 @@
 test("check that the icon exists", function() {
-    var header = document.getElementById('logo');
-    equal(header.alt, 'The Guardian: Time Machine', 'icon is correct');
+  var header = document.getElementById('logo');
+  equal(header.alt, 'The Guardian: Time Machine', 'icon is correct');
 });
 
 
 test("check that the css loads up properly", function() {
-    var header = getComputedStyle(document.getElementById('logo'), null).textAlign;
-    equal(header, 'center', 'css is present');
+  var header = getComputedStyle(document.getElementById('logo'), null).textAlign;
+  equal(header, 'center', 'css is present');
 });
 
 
-test ("test to make sure makeurl is working", function() {
+test("test to make sure makeurl is working", function() {
 	var searchterm = "tennis";
 	var year = "1999";
 	var makeUrlTest = gapi.makeurl(searchterm, year);
@@ -40,33 +40,34 @@ test("check that the guardian is returning content", function(assert) {
 });
 
 test("content is diplayed in the second tab", function(assert){
-   var done = assert.async();
-   document.getElementById('searchTermInput').value = "orange";
-   document.getElementById('yearInput').value = "2001";
-   $( "#go-button" ).trigger( "click" );
-    // waits for the next page to load
-   setTimeout(function() {
-   		$("#next-button").trigger( "click" );
-   		 		setTimeout(function() {
-	    			var hello = document.getElementsByClassName('titles')[1].innerHTML;
-	      			equal(hello,'Nigel Slater: Orange squash');
-	      			done();
-	      		}, 200);
-   		}, 200);
-  });
+  var done = assert.async();
+  document.getElementById('searchTermInput').value = "orange";
+  document.getElementById('yearInput').value = "2001";
+  $( "#go-button" ).trigger( "click" );
+  // waits for the next page to load
+  setTimeout(function() {
+   	$("#next-button").trigger( "click" );
+   	setTimeout(function() {
+	    var hello = document.getElementsByClassName('titles')[1].innerHTML;
+	    equal(hello,'Nigel Slater: Orange squash');
+	    done();
+	  }, 200);
+  }, 200);
+});
 
 test("when go button is clicked placeholder stops cycling suggestions", function(assert) {
-  var placeholderTester = document.getElementById('searchTermInput').innerHTML
-  equal(placeholderTester, "")  
+  var placeholderTester = document.getElementById('searchTermInput').innerHTML;
+  equal(placeholderTester, "");
 });
 
 test("check that ajaxGetRequest get a response from the guardian api", function(assert) {
- var done = assert.async();
- var ajaxTestResponse  = gapi.ajaxGetRequest('http://content.guardianapis.com/search?' + 'from-date=' + "1999" + '-01-01' + '&to-date=' + "1999" + '-12-31' +
+  var done = assert.async();
+  var ajaxTestResponse  = gapi.ajaxGetRequest('http://content.guardianapis.com/search?' + 'from-date=' + "1999" + '-01-01' + '&to-date=' + "1999" + '-12-31' +
       '&order-by-relevance'+'&show-tags=keyword'+'&q=' + "tennis" + '&api-key=2crhgqs3wjpe4vkh9x5j86yt' + "&show-fields=all" + "&show-most-viewed=true", function(){});
- setTimeout(function() {
-             equal( ajaxTestResponse.readyState, 4)  
-              done();
-            }, 200);
-
+  setTimeout(
+    function() {
+      equal(ajaxTestResponse.readyState, 4);
+      done();
+    }, 200
+  );
 });
